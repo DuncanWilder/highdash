f0850712e59e8aaf00dfbfddf5cafb5909de0bb0
Duncan Wilder <duncanawilder@gmail.com>
2025-01-05 17:38:37 +0000

    Prep for find implementation

--- src/find/find.test.ts
+++ src/find/find.test.ts
@@ -1,5 +1,6 @@
-import { it, describe, expect } from 'vitest';
+import { it, describe, expect } from "vitest";
 import find from "./find";
+import _find from "lodash/find";
 
 // Scenarios not covered
 // The `_.matchesProperty` iteratee shorthand.
@@ -12,13 +13,21 @@ interface User {
 	active: boolean;
 }
 
-describe("find", () => {
-	const users: User[] = [
-		{ user: "barney", age: 36, active: true },
-		{ user: "fred", age: 40, active: false },
-		{ user: "pebbles", age: 1, active: true },
-	];
+const users: User[] = [
+	{ user: "barney", age: 36, active: true },
+	{ user: "fred", age: 40, active: false },
+	{ user: "pebbles", age: 1, active: true },
+];
+const arrayOfNumbers = [1, 2, 3, 4, 5];
+const arrayOfStrings = ["a", "b", "c", "d", "e"];
+const testString = "wowzer";
+const testObject = {
+	name: "John",
+	age: 25,
+	active: true,
+};
 
+describe("find", () => {
 	describe("function predicate", () => {
 		it("should return the first thing it finds", () => {
 			expect(find(users, (user: User) => user.age < 40)).toEqual(users[0]);
@@ -46,4 +55,25 @@ describe("find", () => {
 	describe("fromIndex", () => {
 		it.todo("should return the first thing it finds");
 	});
+
+	it.each([
+		// expected, collection, predicate
+		[users[0], users, (user: User) => user.age < 40], // Function on collection returns object
+		[users[2], users, { age: 1, active: true }], // Object on collection returns object
+		['John', testObject, (n) => n === 'John'], // Function on object returns value
+		[undefined, testObject, 'John'], // String on object returns undefined (case insensitive)
+		[undefined, testObject, 'john'], // String on object returns undefined (case insensitive)
+		[undefined, testObject, 'active'], // String on object returns undefined (case insensitive)
+		[undefined, arrayOfNumbers, 1], // Number on array returns undefined
+		[undefined, arrayOfStrings, 'a'], // String on array returns undefined
+		[users[0], users, 'age'], // String on collection returns object (with truthy value?)
+		[undefined, testString, 'w'], // String on string returns undefined
+		['w', testString, (n) => n === 'w'], // Function on string returns value
+	])(
+		'should return "%s" given the collection and predicate',
+		(expected, collection, predicate) => {
+			expect(_find(collection, predicate)).toEqual(expected);
+			expect(find(collection, predicate)).toEqual(expected);
+		},
+	);
 });
--- src/find/find.ts
+++ src/find/find.ts
@@ -5,18 +5,41 @@ type CollectionType = Record<string, any> | string | number;
  * If you are reaching for this function, you probably just want Array.find
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
  */
-export default function find(
-	collection: CollectionType[],
-	predicate:
-		| ((item: CollectionType) => boolean)
-		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-		| Record<string, any>
-		| string
-		| number,
+
+// var users = [
+//   { 'user': 'barney',  'age': 36, 'active': true },
+//   { 'user': 'fred',    'age': 40, 'active': false },
+//   { 'user': 'pebbles', 'age': 1,  'active': true }
+// ];
+
+// _.find(users, function(o) { return o.age < 40; });
+// // => object for 'barney'
+
+// // The `_.matches` iteratee shorthand.
+// _.find(users, { 'age': 1, 'active': true });
+// // => object for 'pebbles'
+
+// // The `_.matchesProperty` iteratee shorthand.
+// _.find(users, ['active', false]);
+// // => object for 'fred'
+
+// // The `_.property` iteratee shorthand.
+// _.find(users, 'active');
+// // => object for 'barney'
+
+export default function find<PassedType>(
+	collection: Record<string, any> | PassedType[],
+	predicate: ((item: PassedType) => boolean) | PassedType | string | number,
 	fromIndex = 0,
 ): CollectionType | undefined {
 	if (typeof predicate === "function") {
-		return collection.find(predicate as (value: CollectionType, index: number, obj: CollectionType[]) => unknown);
+		return collection.find(
+			predicate as (
+				value: CollectionType,
+				index: number,
+				obj: CollectionType[],
+			) => unknown,
+		);
 	}
 
 	const hasBasicArrayBeenPassed =
